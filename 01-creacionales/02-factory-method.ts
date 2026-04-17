e08286dbaafedf0fc72d53f175e4262cd2a183c0
Leandro_Arguello <leandro@innova3ec.com>
2026-04-17 12:11:45 -0500

    hasta el abstract-factory

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,67 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+interface Hamburguer{
+    prepare(): void;
+}
+class ChickenBurger implements Hamburguer{
+    prepare(): void {
+        console.log("Preparando hamburguesa de pollo");
+    }
+}
+class BeefBurger implements Hamburguer{
+    prepare(): void {
+        console.log("Preparando hamburguesa de res");
+    }
+}
+class BeanBurger implements Hamburguer{
+    prepare(): void {
+        console.log("Preparando hamburguesa de frijoles");
+    }
+}
+
+abstract class Restaurant{
+    protected abstract createBurger(): Hamburguer;
+    orderBurger(): void{
+        const burger = this.createBurger();
+        burger.prepare();
+    }
+}
+class ChickenRestaurant extends Restaurant{
+    override createBurger(): Hamburguer {
+        return new ChickenBurger();
+    }
+}
+class BeefRestaurant extends Restaurant{
+    override createBurger(): Hamburguer {
+        return new BeefBurger();
+    }
+}
+class BeanRestaurant extends Restaurant{
+    override createBurger(): Hamburguer {
+        return new BeanBurger();
+    }
+}
+
+function main(){
+
+   let restaurant: Restaurant;
+   const burguerType = prompt("¿Qué tipo de hamburguesa deseas? (pollo/res/frijoles)");
+   switch(burguerType){
+       case "pollo":
+           restaurant = new ChickenRestaurant();
+           break;
+       case "res":
+           restaurant = new BeefRestaurant();
+           break;
+       case "frijoles":
+           restaurant = new BeanRestaurant();
+           break;
+       default:
+           throw new Error("Tipo de hamburguesa no válido");
+   }
+   restaurant.orderBurger();
+}
+
+main();
+
